c13558e540f1041ee4e42d118619fb8a6234cf31
Dunspixel <53792348+Dunspixel@users.noreply.github.com>
2025-10-01 23:49:28 +0100

    Some time has passed

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ function buildFooter()
 	p1.innerText = "© 2024-2025 Dunspixel";
 
 	var p2 = document.createElement("p");
-	p2.innerText = "Last Updated 24/07/2025";
+	p2.innerText = "Last Updated 01/10/2025";
 
 	var p3 = document.createElement("p");
 	var githubLink = document.createElement("a");
